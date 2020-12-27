be22011ec055151a6455626deb17db1beb318227
augustoavg <augustoavilag@hotmail.com>
2020-12-26 23:20:22 -0300

    feat/update-driver

---
+++ src/modules/Driver/__tests__/Integration/UpdateDriver.spec.ts
@@ -0,0 +1,42 @@
+import request from 'supertest';
+import Driver from '../../infra/mongoose/schemas/Driver';
+import MongoMock from '../../../../shared/__tests__/MongoMock';
+import app from '../../../../app';
+
+describe('Update a new - Integration', () => {
+  beforeAll(async () => {
+    await MongoMock.connect();
+  });
+
+  beforeEach(async () => {
+    await Driver.deleteMany({});
+  });
+
+  afterAll(async () => {
+    await MongoMock.disconnect();
+  });
+
+  it('should be able to update a driver', async () => {
+    const driverData = {
+      taxId: '12345678910',
+      name: 'test',
+    };
+
+    await Driver.create(driverData);
+
+    const response = await request(app)
+      .post(`/driver/${driverData.taxId}`)
+      .send({ name: 'new test' });
+
+    expect(response.status).toBe(200);
+    expect(response.body.name).toBe('new test');
+  });
+
+  it('should not be able to update a driver if does not exists', async () => {
+    const response = await request(app)
+      .post(`/driver/1`)
+      .send({ name: 'test' });
+
+    expect(response.status).toBe(400);
+  });
+});
